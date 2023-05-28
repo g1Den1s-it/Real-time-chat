f3aa60494da24075be9226ec8b8a86d614236017
g1Den1s-it <g1den1splay@gmail.com>
2023-05-28 22:30:59 +0300

    added static dirs and dirs templates

--- real_time_chat/settings.py
+++ real_time_chat/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'real_time_chat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
