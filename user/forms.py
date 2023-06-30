f769f793f011296bc01894e26da59ca27c58ea9a
g1Den1s-it <g1den1splay@gmail.com>
2023-06-30 12:04:21 +0300

    some cleaning

--- user/forms.py
+++ user/forms.py
@@ -8,16 +8,30 @@ class LoginForm(forms.Form):
     tag = forms.CharField(widget=forms.TextInput())
     password = forms.CharField(widget=forms.PasswordInput())
 
-    # class Meta:
-    #     model = User
-    #     fields = ['tag','password']
-
 
 class EditProfileForm(forms.ModelForm):
     """form for edit user profile"""  
+    image = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class': 'profile-wrapper-form-image', 'placeholder': 'put Image'}))
+    tag = forms.CharField(widget=forms.TextInput(attrs={'class': 'profile-wrapper-form-input', 'placeholder': 'Enter Tag'}))
+    username = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'profile-wrapper-form-input', 'placeholder': 'Enter Username'}))
+    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={'class': 'profile-wrapper-form-input', 'placeholder': 'Enter Email'}))
+    first_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'profile-wrapper-form-input', 'placeholder': 'Enter First name'}))
+    last_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'profile-wrapper-form-input', 'placeholder': 'Enter Last name'}))
+
     class Meta:
         model = User
-        exclude = ('user_permissions', 'groups', 'password', 'is_superuser', 'is_staff', 'is_active', 'date_joined')
+        fields = ("image", 'tag', 'username', 'email','first_name', 'last_name')
+    
+    def clean(self):
+        cleaned_data = super().clean()
+
+        for field_name in self.fields:
+            value = cleaned_data.get(field_name)
+            if value == '':
+                cleaned_data[field_name] = ''
+
+        return cleaned_data
+
 
 
 class RegisterForm(forms.ModelForm):
@@ -31,3 +45,5 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'tag', 'email','password1', 'password2']
+
+
